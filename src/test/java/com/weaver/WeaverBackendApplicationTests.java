f64ebb3ce93b2f3c6bc47e5160ebf7f50df2c65b
DongWon Lee <atropic159@hanyang.ac.kr>
2026-04-09 20:38:16 +0900

    chore(init): 도메인 패키지 구조 생성 및 프로젝트 초기 설정

---
+++ src/test/java/com/weaver/WeaverBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeaverBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
