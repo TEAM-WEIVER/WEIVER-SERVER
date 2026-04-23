4f67a63edc5d3eb28bb65faeb39fb403785b4f57
LeeHyunWoo02 <hyoun0824@naver.com>
2026-04-23 16:46:48 +0900

    feat(company) : 기업 공고 조회 API DTO 작성

--- src/main/java/com/weiver/company/dto/response/CompanyDashboardResponseDTO.java
+++ src/main/java/com/weiver/company/dto/response/CompanyDashboardResponseDTO.java
@@ -1,8 +1,31 @@
 package com.weiver.company.dto.response;
 
+import com.weiver.company.domain.Company;
+
 public record CompanyDashboardResponseDTO(
         long companyId,
         String companyLogoUrl,
-        String companyCeoName
+        String companyCeoName,
+        String address,
+        Integer employeeNum,
+        String foundedYear,
+        WayOfWorkingDetail wayOfWorkingDetail
 ) {
+    public static CompanyDashboardResponseDTO from(Company company){
+        return new CompanyDashboardResponseDTO(
+                company.getCompanyId(),
+                company.getCompanyLogoUrl(),
+                company.getCompanyCeoName(),
+                company.getAddress(),
+                company.getEmployeeNum(),
+                company.getFoundedYear() != null ? company.getFoundedYear().toString() : null,
+
+                new WayOfWorkingDetail(
+                        company.getWorkPace().getDescription(),
+                        company.getDecisionMaking().getDescription(),
+                        company.getRoleDefinition().getDescription(),
+                        company.getOperationStyle().getDescription()
+                )
+        );
+    }
 }
---
+++ src/main/java/com/weiver/company/dto/response/WayOfWorkingDetail.java
@@ -0,0 +1,8 @@
+package com.weiver.company.dto.response;
+
+public record WayOfWorkingDetail(
+        String workPace,
+        String decisionMaking,
+        String roleDefinition,
+        String operationStyle
+) {}
