9142abcfdcb1ac2710105778420a0ac8c2a9a74c
DongWon Lee <atropic159@hanyang.ac.kr>
2026-04-26 21:03:12 +0900

    refactor: @NonNull 어노테이션 추가

--- src/main/java/com/weiver/global/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/weiver/global/security/jwt/JwtAuthenticationFilter.java
@@ -9,6 +9,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -28,9 +29,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            FilterChain filterChain
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
         try {
             String accessToken = bearerTokenResolver.resolve(request);
