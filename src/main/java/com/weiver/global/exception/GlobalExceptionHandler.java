5dd6e855484bdc08127063871ab2b6b1e883ffe5
LeeHyunWoo02 <hyoun0824@naver.com>
2026-04-27 12:20:50 +0900

    fix(global) : conflict 충돌 해결

--- src/main/java/com/weiver/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/weiver/global/exception/GlobalExceptionHandler.java
@@ -199,11 +199,4 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return List.of();
     }
 
-    // 날짜 형식이 틀리거나(2021-99-33) JSON 파싱이 안 될 때 발생하는 예외 처리
-    @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
-        return ResponseEntity
-                .badRequest()
-                .body("날짜 형식이 올바르지 않거나 존재하지 않는 날짜입니다.");
-    }
 }
