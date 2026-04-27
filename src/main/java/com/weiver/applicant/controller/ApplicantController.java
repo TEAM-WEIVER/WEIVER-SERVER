e95f159a60ad5a5d88c1bb0b6ecadf0d9c28c3c5
LeeHyunWoo02 <hyoun0824@naver.com>
2026-04-27 20:11:06 +0900

    refeact(applicant) : Controller 가독성 향상 메소드 순서 변경

--- src/main/java/com/weiver/applicant/controller/ApplicantController.java
+++ src/main/java/com/weiver/applicant/controller/ApplicantController.java
@@ -30,7 +30,6 @@ public class ApplicantController {
                                                                  @RequestPart(value = "profileImage", required = false) MultipartFile profileImage,
                                                                Principal principal){
         Long applicantId = extractedId(principal);
-
         applicantService.updateApplicantInfo(applicantId, requestDTO, profileImage);
 
         return ResponseEntity.ok(ApiResponse.success("개인정보 저장에 성공했습니다."));
@@ -40,20 +39,36 @@ public class ApplicantController {
     public ResponseEntity<ApiResponse<Void>> saveEducationInfo(@RequestBody @Valid EducationRequestDTO requestDTO,
                                                                Principal principal){
         Long applicantId = extractedId(principal);
-        
         applicantService.saveEducationInfo(applicantId, requestDTO);
 
         return ResponseEntity.ok(ApiResponse.success("학력 저장에 성공했습니다."));
+    }
+
+    @PutMapping("/education")
+    public ResponseEntity<ApiResponse<Void>> updateEducationInfo(@RequestBody @Valid EducationUpdateRequestDTO requestDTO,
+                                                                 Principal principal){
+        Long applicantId = extractedId(principal);
+        applicantService.updateEducationInfo(applicantId, requestDTO);
+
+        return ResponseEntity.ok(ApiResponse.success("학력 업데이트 성공했습니다."));
     }
 
     @PostMapping("/award")
     public ResponseEntity<ApiResponse<Void>> saveAwardInfo(@RequestBody @Valid AwardRequestDTO requestDTO,
                                                            Principal principal){
         Long applicantId = extractedId(principal);
-
         applicantService.saveAwardInfo(applicantId, requestDTO);
 
         return ResponseEntity.ok(ApiResponse.success("수상이력 저장에 성공했습니다."));
+    }
+
+    @PutMapping("/award")
+    public ResponseEntity<ApiResponse<Void>> updateAwardInfo(@RequestBody @Valid AwardUpdateRequestDTO requestDTO,
+                                                             Principal principal){
+        Long applicantId = extractedId(principal);
+        applicantService.updateAwardInfo(applicantId, requestDTO);
+
+        return ResponseEntity.ok(ApiResponse.success("수상목록 업데이트 성공했습니다."));
     }
 
     @PostMapping("/certificate")
@@ -65,6 +80,15 @@ public class ApplicantController {
         return ResponseEntity.ok(ApiResponse.success("자격증 저장에 성공했습니다."));
     }
 
+    @PutMapping("/certificate")
+    public ResponseEntity<ApiResponse<Void>> updateCertificateInfo(@RequestBody @Valid CertificateUpdateRequestDTO requestDTO,
+                                                                   Principal principal){
+        Long applicantId = extractedId(principal);
+        applicantService.updateCertificateInfo(applicantId, requestDTO);
+
+        return ResponseEntity.ok(ApiResponse.success("자격증 목록 업데이트 성공했습니다."));
+    }
+
     @PostMapping("/experience")
     public ResponseEntity<ApiResponse<Void>> saveWorkExperienceInfo(@RequestBody @Valid WorkExperienceRequestDTO requestDTO,
                                                                     Principal principal){
@@ -74,36 +98,12 @@ public class ApplicantController {
         return ResponseEntity.ok(ApiResponse.success("경력 저장에 성공했습니다."));
     }
 
-
-    @PutMapping("/award")
-    public ResponseEntity<ApiResponse<Void>> updateAwardInfo(@RequestBody @Valid AwardUpdateRequestDTO requestDTO,
-                                                             Principal principal){
-        Long applicantId = extractedId(principal);
-        applicantService.updateAwardInfo(applicantId, requestDTO);
-        return ResponseEntity.ok(ApiResponse.success("수상목록 업데이트 성공했습니다."));
-    }
-
-    @PutMapping("/certificate")
-    public ResponseEntity<ApiResponse<Void>> updateCertificateInfo(@RequestBody @Valid CertificateUpdateRequestDTO requestDTO,
-                                                             Principal principal){
-        Long applicantId = extractedId(principal);
-        applicantService.updateCertificateInfo(applicantId, requestDTO);
-        return ResponseEntity.ok(ApiResponse.success("자격증 목록 업데이트 성공했습니다."));
-    }
-
-    @PutMapping("/education")
-    public ResponseEntity<ApiResponse<Void>> updateEducationInfo(@RequestBody @Valid EducationUpdateRequestDTO requestDTO,
-                                                             Principal principal){
-        Long applicantId = extractedId(principal);
-        applicantService.updateEducationInfo(applicantId, requestDTO);
-        return ResponseEntity.ok(ApiResponse.success("학력 업데이트 성공했습니다."));
-    }
-
     @PutMapping("/experience")
     public ResponseEntity<ApiResponse<Void>> updateExperienceInfo(@RequestBody @Valid WorkExperienceUpdateRequestDTO requestDTO,
                                                              Principal principal){
         Long applicantId = extractedId(principal);
         applicantService.updateWorkExperienceInfo(applicantId, requestDTO);
+
         return ResponseEntity.ok(ApiResponse.success("경력 업데이트 성공했습니다."));
     }
 
