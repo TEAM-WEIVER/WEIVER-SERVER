e2870d3d77fe8c4b4877b69eceb14d389c753ec6
LeeHyunWoo02 <hyoun0824@naver.com>
2026-04-28 16:32:40 +0900

    refeact(S3) : S3ServiceImpl 최신 버전으로 리펙토링

--- src/main/java/com/weiver/global/s3/service/S3ServiceImpl.java
+++ src/main/java/com/weiver/global/s3/service/S3ServiceImpl.java
@@ -1,12 +1,9 @@
 package com.weiver.global.s3.service;
 
-import com.amazonaws.HttpMethod;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.weiver.global.exception.BusinessException;
 import com.weiver.global.exception.ErrorCode;
+import io.awspring.cloud.s3.ObjectMetadata;
+import io.awspring.cloud.s3.S3Template;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -19,7 +16,7 @@ import java.io.InputStream;
 import java.net.URL;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
-import java.util.Date;
+import java.time.Duration;
 import java.util.UUID;
 
 @Slf4j
@@ -27,7 +24,7 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class S3ServiceImpl implements S3Service {
 
-    private final AmazonS3 amazonS3;
+    private final S3Template s3Template;
 
     @Value("${spring.cloud.aws.s3.bucket.public}")
     private String publicBucket;
@@ -55,20 +52,9 @@ public class S3ServiceImpl implements S3Service {
             String targetBucket = fileUrl.contains(privateBucket) ? privateBucket : publicBucket;
             String objectKey = extractObjectKey(fileUrl);
 
-            Date expiration = new Date();
-            long expTimeMillis = expiration.getTime();
-            expTimeMillis += 1000 * 60 * 30; // 30분
-            expiration.setTime(expTimeMillis);
+            URL presignedUrl = s3Template.createSignedGetURL(targetBucket, objectKey, Duration.ofMinutes(30));
 
-            // 임시 접근 권한이 담긴 URL 생성 요청
-            GeneratePresignedUrlRequest generatePresignedUrlRequest =
-                    new GeneratePresignedUrlRequest(targetBucket, objectKey)
-                            .withMethod(HttpMethod.GET)
-                            .withExpiration(expiration);
-
-            // 생성된 URL 반환
-            URL url = amazonS3.generatePresignedUrl(generatePresignedUrlRequest);
-            return url.toString();
+            return presignedUrl.toString();
 
         } catch (Exception e) {
             log.error("Presigned URL 생성 실패 (URL: {}): {}", fileUrl, e.getMessage(), e);
@@ -85,18 +71,17 @@ public class S3ServiceImpl implements S3Service {
         String extension = StringUtils.getFilenameExtension(originalFilename);
         String uniqueFileName = dirName + "/" + UUID.randomUUID() + "." + extension;
 
-        ObjectMetadata metadata = new ObjectMetadata();
-        metadata.setContentType(file.getContentType());
-        metadata.setContentLength(file.getSize());
+        ObjectMetadata metadata = ObjectMetadata.builder()
+                .contentType(file.getContentType())
+                .build();
 
         try (InputStream inputStream = file.getInputStream()) {
-            amazonS3.putObject(new PutObjectRequest(targetBucket, uniqueFileName, inputStream, metadata));
+            URL url = s3Template.upload(targetBucket, uniqueFileName, inputStream, metadata).getURL();
+            return url.toString();
         } catch (IOException e) {
             log.error("S3 업로드 실패: {}", e.getMessage(), e);
             throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR, "파일 업로드에 실패했습니다.");
         }
-
-        return amazonS3.getUrl(targetBucket, uniqueFileName).toString();
     }
 
     @Override
@@ -107,12 +92,9 @@ public class S3ServiceImpl implements S3Service {
 
         try {
             String targetBucket = fileUrl.contains(privateBucket) ? privateBucket : publicBucket;
+            String decodedKey = extractObjectKey(fileUrl);
 
-            URL url = new URL(fileUrl);
-            String objectKey = url.getPath().substring(1);
-            String decodedKey = URLDecoder.decode(objectKey, StandardCharsets.UTF_8);
-
-            amazonS3.deleteObject(targetBucket, decodedKey);
+            s3Template.deleteObject(targetBucket, decodedKey);
 
         } catch (Exception e) {
             log.error("S3 파일 삭제 실패 (URL: {}): {}", fileUrl, e.getMessage(), e);
