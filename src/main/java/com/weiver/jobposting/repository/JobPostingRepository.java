3d5ec99b69e47758dce25032ab1a63de30c8d8fc
LeeHyunWoo02 <hyoun0824@naver.com>
2026-05-04 21:51:39 +0900

    feat(jobPosting) : 기업 공고 상태 변화 로직 스케쥴러 활용 로직 추가

--- src/main/java/com/weiver/jobposting/repository/JobPostingRepository.java
+++ src/main/java/com/weiver/jobposting/repository/JobPostingRepository.java
@@ -5,10 +5,20 @@ import com.weiver.jobposting.type.JobPostingStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
 
 @Repository
 public interface JobPostingRepository extends JpaRepository<JobPosting, Long> {
     Page<JobPosting> findByCompany_CompanyId(Long companyId, Pageable pageable);
     Page<JobPosting> findByCompany_CompanyIdAndStatus(Long companyId, JobPostingStatus status, Pageable pageable);
+
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE JobPosting j SET j.status = 'CLOSED' " +
+            "WHERE j.status = 'ACTIVE' AND j.deadline < :now")
+    int closeExpiredJobPostings(@Param("now") LocalDateTime now); // deadline 기준으로 status 변경
 }
---
+++ src/main/java/com/weiver/jobposting/service/JobPostingScheduler.java
@@ -0,0 +1,31 @@
+package com.weiver.jobposting.service;
+
+import com.weiver.jobposting.repository.JobPostingRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+import static reactor.netty.http.HttpConnectionLiveness.log;
+
+
+@Component
+@RequiredArgsConstructor
+public class JobPostingScheduler {
+
+    private final JobPostingRepository jobPostingRepository;
+
+    // 매일 밤 12시 0분 0초에 실행
+    @Scheduled(cron = "0 0 0 * * *", zone = "Asia/Seoul")
+    @Transactional
+    public void closeExpiredJobPostings() {
+        log.info("마감일이 지난 공고를 CLOSED 상태로 변경합니다.");
+
+        LocalDateTime now = LocalDateTime.now();
+        int updatedCount = jobPostingRepository.closeExpiredJobPostings(now);
+
+        log.info("총 {}개의 공고가 CLOSED로 변경되었습니다.", updatedCount);
+    }
+}
