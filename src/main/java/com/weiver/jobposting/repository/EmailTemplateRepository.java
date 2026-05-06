803f2289b917992bbf5c68f3989a54b3ae7cd0c5
LeeHyunWoo02 <hyoun0824@naver.com>
2026-05-06 17:51:34 +0900

    feat(JobPosting) : queryDSL 으로 변경

--- src/main/java/com/weiver/jobposting/repository/EmailTemplateRepository.java
+++ src/main/java/com/weiver/jobposting/repository/EmailTemplateRepository.java
@@ -2,14 +2,9 @@ package com.weiver.jobposting.repository;
 
 import com.weiver.jobposting.domain.EmailTemplate;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 
 @Repository
-public interface EmailTemplateRepository extends JpaRepository<EmailTemplate, Long> {
-    @Query("SELECT e FROM EmailTemplate e JOIN FETCH e.jobPosting WHERE e.jobPosting.jdId = :jdId")
-    Optional<EmailTemplate> findWithJobPostingByJdId(@Param("jdId") Long jdId);
+public interface EmailTemplateRepository extends JpaRepository<EmailTemplate, Long>, EmailTemplateRepositoryCustom {
 }
