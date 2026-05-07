bd32ec17a0d0f0dbf93a005e0f2e0917ebf3f17a
DongWon Lee <144747366+DongWonLee2@users.noreply.github.com>
2026-05-07 23:55:57 +0900

    fix: swagger 설정에 서버 url 명시하여 mixed content 해결 (#54)

--- src/main/java/com/weiver/global/config/SwaggerConfig.java
+++ src/main/java/com/weiver/global/config/SwaggerConfig.java
@@ -5,7 +5,12 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @OpenAPIDefinition(
         info = @Info(
@@ -23,4 +28,12 @@ import org.springframework.context.annotation.Configuration;
 )
 @Configuration
 public class SwaggerConfig {
+
+        @Bean
+        public OpenAPI openAPI() {
+                return new OpenAPI()
+                        .servers(List.of(
+                                new Server().url("https://api.piuda.site").description("Production Server")
+                        ));
+        }
 }
