ab7634f29e42eb14b783a000a756be6b577497d5
DongWon Lee <atropic159@hanyang.ac.kr>
2026-05-30 18:35:24 +0900

    fix: NotBlank를 추가하여 NPE 방지

--- src/main/java/com/weiver/global/event/config/RabbitMqProperties.java
+++ src/main/java/com/weiver/global/event/config/RabbitMqProperties.java
@@ -1,14 +1,28 @@
 package com.weiver.global.event.config;
 
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
+@Validated
 @ConfigurationProperties(prefix = "weiver.rabbitmq")
 public record RabbitMqProperties(
+        @NotBlank
         String domainExchange,
+
+        @NotBlank
         String deadLetterExchange,
+
+        @NotBlank
         String springQueue,
+
+        @NotBlank
         String springDlq,
+
+        @NotBlank
         String aiQueue,
+
+        @NotBlank
         String aiDlq
 ) {
 }
